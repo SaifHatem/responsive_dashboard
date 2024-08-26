4f7648ea10656cba28f0b9a4e1b5fe514f53a997
Saif Hatem <shatem793@gmail.com>
2024-08-27 00:33:36 +0300

    generate assets

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for imagesCardBackground
+  /// assets/images/cardBackground.svg
+  static const String imagesCardBackground = "assets/images/cardBackground.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMoneys
+  /// assets/images/moneys.svg
+  static const String imagesMoneys = "assets/images/moneys.svg";
+
+  /// Assets for imagesMyInvestment
+  /// assets/images/my_investment.svg
+  static const String imagesMyInvestment = "assets/images/my_investment.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesTransaction
+  /// assets/images/transaction.svg
+  static const String imagesTransaction = "assets/images/transaction.svg";
+
+  /// Assets for imagesWallet
+  /// assets/images/wallet.svg
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
